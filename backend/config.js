5a864220b9bd85f05ea6246a9c5c0afa8d951918
Rubén <rubenmartinezTRABALLOS@gmail.com>
2023-11-18 19:53:16 +0100

    gitignore added

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,6 @@
+require('dotenv').config()
+
 export const PORT = 5555
 
-export const mongoDBURL = 
-    'mongodb+srv://root:MS4BKiSVm4WgamCd@books-store-mern.bobcc1s.mongodb.net/?retryWrites=true&w=majority'
+export const mongoDBURL =
+    `mongodb+srv://${process.env.USER}:${process.env.API_KEY}@books-store-mern.bobcc1s.mongodb.net/?retryWrites=true&w=majority`
